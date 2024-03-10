0c95f5f898ef97b3a838235433890a4b7c5e6b0b
dilyaraabd <211721@astanait.edu.kz>
2024-03-10 22:37:02 +0600

    routes have been created

---
+++ lib/routes/routing.dart
@@ -0,0 +1,44 @@
+
+import 'package:diploma_work/main_pages/weather_page.dart';
+import 'package:diploma_work/widgets/navbar.dart';
+import 'package:go_router/go_router.dart';
+
+
+import '../authorization_pages/Registr.dart';
+import '../authorization_pages/login.dart';
+import '../main_pages/profile.dart';
+import '../main_pages/recommendation_page.dart';
+import '../main_pages/style_page.dart';
+
+final router = GoRouter(
+  initialLocation: '/weather',
+  routes: [
+
+    GoRoute(path: '/style', builder: (context, state) =>  StylePage(),),
+    GoRoute(path: '/login', builder: (context, state) =>  LoginPage(),),
+    GoRoute(path: '/registr', builder: (context, state) =>  RegistrationPage(),),
+
+
+
+    // BottomNavigationBar
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) =>
+          Navbar(navigationShell: navigationShell),
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(path: '/weather', builder: (context, state) =>  WeatherPage(),),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [GoRoute(path: '/recommendation', builder: (context, state) =>  RecommendationPage(),),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [GoRoute(path: '/profile', builder: (context, state) =>  ProfilePage(),),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
